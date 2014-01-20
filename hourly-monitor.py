7c79d32a0b2f4ab45d61278c575f78ffc59a7b0d
Luis Emiro <luis.linares@cern.ch>
2014-01-20 17:46:33 -0500

    Fixed ISP to Site assignment problem

--- hourly-monitor.py
+++ hourly-monitor.py
@@ -30,7 +30,7 @@ def main():
     config = json.load( open(config_file))
     json.dump(config, open(config_file, 'w'), indent=3)
 
-    groups = config['groups'] 
+    groups = config['groups']
     for machine_group_name in groups.keys():
         analyze_failovers_to_group( config, machine_group_name, geo, squids, geoip)
 
@@ -66,8 +66,11 @@ def analyze_failovers_to_group (config, groupname, geo, squids, geoip):
         offending = None
         totals_high = None
 
-    gen_report (groupname, geo, offending, totals_high)
-    update_record (record_file, offending, now, record_span)
+    geolist_func = lambda s: s.encode('utf-8', 'ignore').replace(' ', '')
+    get_sites = lambda inst: ', '.join( geo[ geo['Institution'] == geolist_func(inst) ]['Site'].unique().tolist() )
+
+    gen_report (groupname, geo, offending, totals_high, get_sites)
+    update_record (record_file, offending, now, record_span, get_sites)
 
     return 0
 
@@ -133,7 +136,7 @@ def excess_failover_check (awdata, squids, site_rate_threshold):
 
     return offending, totals_high
 
-def gen_report (groupname, geo, offending, totals_high):
+def gen_report (groupname, geo, offending, totals_high, sites_function):
 
     print "Failover activity to %s:" % groupname
 
@@ -141,36 +144,34 @@ def gen_report (groupname, geo, offending, totals_high):
         print " None.\n"
         return
 
-    geolist_func = lambda s: s.encode('utf-8', 'ignore').replace(' ', '')
-    get_sites = lambda inst: ', '.join( geo[ geo['Institution'] == geolist_func(inst) ]['Site'].unique().tolist() )
     for_report = offending.copy()
 
     if len(for_report) > 0:
-        for_report['Sites'] = for_report['Institution'].apply(get_sites)
+        for_report['Sites'] = for_report['Institution'].apply(sites_function)
         print for_report.set_index(['Institution', 'IsSquid', 'Host']).sortlevel(0), "\n"
 
     else:
         print " None.\n"
 
-def update_record (record_file, new_data, now, record_span):
+def update_record (record_file, new_data, now, record_span, sites_function):
 
     time_field = 'Timestamp'
 
     now_secs = datetime_to_UTC_epoch(now)
 
-    if new_data is None: 
+    if new_data is None:
         return
 
     if os.path.exists(record_file):
         records = pd.read_csv(record_file)
-	old_cutoff = now_secs - record_span*3600
+        old_cutoff = now_secs - record_span*3600
         records = records[ records[time_field] >= old_cutoff ]
 
     else:
         records = None
 
     to_add = new_data.copy()
-    to_add['Sites'] = to_add['Institution'].apply(get_sites)
+    to_add['Sites'] = to_add['Institution'].apply(sites_function)
     to_add = to_add.drop('Institution', axis=1)
     to_add.insert(0, time_field, now_secs)
 
@@ -180,7 +181,7 @@ def update_record (record_file, new_data, now, record_span):
         update = to_add
 
     update.to_csv(record_file, index=False)
-    
+
 
 if __name__ == "__main__":
     sys.exit(main())
