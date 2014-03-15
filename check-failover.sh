5102dee906d319d78e412feb776d3f42949a5c39
Luis Emiro <luis.linares@cern.ch>
2014-03-15 14:18:02 +0100

    Added backup of current record files

--- check-failover.sh
+++ check-failover.sh
@@ -9,6 +9,11 @@ if [ -z "${CUSTOM_PYTHON_VIRTUALENV}" ]; then
     exit 1
 fi
 
+record_files=$( grep -o '[^"]*\.csv' $here/instance_config.json )
+for file in ${record_files}; do
+    cp $file $file.0
+done
+
 echo "Failover Monitor starting at $(date)"
 echo
 ${CUSTOM_PYTHON_VIRTUALENV}/bin/python -W ignore::DeprecationWarning $here/hourly-monitor.py
