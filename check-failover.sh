d9a902e2f9100eb2352702d5e153ade13e36f7a0
Luis Emiro <luis.linares@cern.ch>
2014-06-22 16:00:05 +0200

    Fixed renaming inconsistency of JSON config file

--- check-failover.sh
+++ check-failover.sh
@@ -3,13 +3,15 @@
 source $HOME/.bash_profile
 
 here=$( cd $(dirname $0); pwd -P )
+config_file="${here}/config.json"
+
 
 if [ -z "${CUSTOM_PYTHON_VIRTUALENV}" ]; then
     echo "Please set the path to the custom Python virtualenv is in the CUSTOM_PYTHON_VIRTUALENV variable."
     exit 1
 fi
 
-record_files=$( grep -o '[^"]*\.csv' $here/instance_config.json )
+record_files=$( grep -o '[^"]*\.csv' ${config_file} )
 for file in ${record_files}; do
     cp $file $file.0
 done
@@ -18,7 +20,7 @@ echo
 echo "========================================================="
 echo "Failover Monitor starting at $(date)"
 echo
-${CUSTOM_PYTHON_VIRTUALENV}/bin/python -W ignore::DeprecationWarning $here/hourly-monitor.py $here/config.json
+${CUSTOM_PYTHON_VIRTUALENV}/bin/python -W ignore::DeprecationWarning $here/hourly-monitor.py ${config_file} 
 echo
 echo "Failover Monitor done at $(date)"
 
