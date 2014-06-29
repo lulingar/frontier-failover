f604ba53e66d7eddcd73e212516a201eab9bcef7
Luis Emiro <luis.linares@cern.ch>
2014-06-30 01:06:31 +0200

    Basic site filtering through URL param

--- web/js/failover.lib.js
+++ web/js/failover.lib.js
@@ -88,6 +88,7 @@ function getUrlVars()
         for(var i = 0; i < hashes.length; i++)
         {
             hash = hashes[i].split('=');
+            vars.push(hash[0]);
             vars[hash[0]] = decodeURI(hash[1]);
         }
     }
